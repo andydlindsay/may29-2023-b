b8c6edbb283dfda06e5d58e456e2ee9021c776d7
Christian Nally <sticksallison@gmail.com>
2023-06-06 12:07:24 -0700

    final version of the robot

--- w02d2/sample-robot.js
+++ w02d2/sample-robot.js
@@ -1,23 +1,91 @@
-const start = Date.now();
+//               'getUp', 3000, walk
+function doAction(name, duration, nextAction){
+  console.log(`Starting the ${name} action.`);
+  setTimeout(() => {
+    console.log(`Ending the ${name} action.`);
+    if (nextAction){
+      nextAction();
+    }
+  }, duration); // schedules a callback to be called
+}
 
 //
-// Look
+// look
 //
+const look = () => {
+  doAction('look', 1500, look);
+};
 
 //
-// Get Up
+// getUp
 //
+const getUp = () => {
+  doAction('getUp', 3000, walk);
+};
 
 //
-// Walk
+// walk
 //
+const walk = () => {
+  doAction('walk', 5000, openTheDoor);
+};
 
 //
 // openTheDoor
 //
+const openTheDoor = () => {
+  doAction('openTheDoor', 2000, walkThroughTheDoor);
+};
 
 //
 // walkThroughTheDoor
 //
+const walkThroughTheDoor = () => {
+  doAction('walkThroughTheDoor', 1500, null);
+};
 
-console.log("I am done being programmed.");
+
+look();
+getUp();
+sleepFor(10000);
+
+console.log("End of the Main Thread.");
+
+// the event loop cannot start until the main thread is finished.
+
+// const fs = require('fs');
+
+// fs.readFile('./filename.txt', 'utf8', (err, results) => {
+//   if (err){
+//     console.log('File Read Error:', err);
+//   } else {
+//     console.log('results', results);
+//   }
+
+// });
+
+// const axios = require('axios');
+
+// axious.get('http://example.com/monkeyfuzz', );
+
+// // 
+// // callbacks are not in and of themselves async
+// // 
+
+// const one = () => {
+//   console.log('one');
+// };
+
+// const two = () => {
+//   console.log('two');
+// };
+
+
+// function doThings(callback){
+//   for (let j = 0; j < 42; j++) {
+//     callback();
+//   }
+// }
+
+// doThings(two);
+
