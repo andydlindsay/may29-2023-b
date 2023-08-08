e83b2f35b1c45a747ec3c71d83cf3ea1ee8daff1
Andy Lindsay <andydlindsay@gmail.com>
2023-08-08 15:32:22 -0700

    breakout over

--- breakout-react-router/router-demo/src/App.js
+++ breakout-react-router/router-demo/src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
 
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+
+import About from './components/About';
+import Products from './components/Products';
+import Home from './components/Home';
+import Product from './components/Product';
+import Nav from './components/Nav';
+
 const App = () => {
   return (
     <div className="App">
       <h2>React Router Demo</h2>
+
+      <BrowserRouter>
+        <Nav />
+      
+        <Routes>
+          <Route path="*" element={<Navigate to="/about" />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/admin" element={<h2>Admin Page</h2>} />
+          <Route path="/products/:productId" element={<Product />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
